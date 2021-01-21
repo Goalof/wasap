ce6f9fa2fec750e992cd3c849ca4c2d95ce5d201
Goalof <68434460+Goalof@users.noreply.github.com>
2021-01-22 02:04:15 +0600

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text } from "@quarkly/widgets";
+import { Theme, Link, Text, Icon, Box, Input, Button } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { RawHtml, Override, Menu, Section } from "@quarkly/components";
+import { RawHtml, Override, Menu, Section, StackItem, Stack, Formspree, SocialMedia } from "@quarkly/components";
+import { MdHelp } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -32,6 +33,239 @@ export default (() => {
 			</Menu>
 		</Section>
 		<Section padding="400px 0 400px 0" background="#edacac" />
+		<Section color="--dark">
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Q&A
+			</Text>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Is there life on Mars?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0 0 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					How much salt is in the ocean?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+			<Box
+				margin="36px 0"
+				padding="0 0 0 54px"
+				sm-padding="54px 0 0 0"
+				position="relative"
+				max-width="800px"
+			>
+				<Icon
+					position="absolute"
+					top="0"
+					left="0"
+					size="36px"
+					color="--primary"
+					category="md"
+					icon={MdHelp}
+				/>
+				<Text as="h3" font="--headline3" margin="10px 0">
+					Why is the sky blue?
+				</Text>
+				<Text as="p" font="--lead" margin="10px 0" color="--greyD2">
+					It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+				</Text>
+			</Box>
+		</Section>
+		<Section background="--color-light" color="--dark" padding="64px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						margin="0"
+					>
+						Contact
+					</Text>
+					<Text font="--headline2" max-width="500px" margin="10px 0 0 0">
+						Email us, we would love to hear form you
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Formspree>
+						<Stack gap="16px">
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Name
+								</Text>
+								<Input max-width="400px" width="100%" name="name" />
+							</StackItem>
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Email
+								</Text>
+								<Input max-width="400px" width="100%" type="email" name="email" />
+							</StackItem>
+							<StackItem width="100%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Message
+								</Text>
+								<Input as="textarea" rows="4" width="100%" name="message" />
+							</StackItem>
+							<StackItem width="100%">
+								<Button>
+									Send
+								</Button>
+							</StackItem>
+						</Stack>
+					</Formspree>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m David
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Stack color="--grey" font="--base">
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px" font="--headline1" md-font="--headline2" color="--dark">
+						17
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						color="--dark"
+						padding="98px 64px"
+						background="--color-light"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="12px 0"
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About Zanzibar
+					</Text>
+					<Text as="h2" margin="12px 0" font="--headline2" md-font="--headline3">
+						This is one of the world's greatest cultural crossroads where Africa meets the Indian Ocean.
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Stack>
+						<StackItem width="100%" lg-width="33.3%" md-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999080555-3f7a698dd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1577315734214-4b3dec92d9ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999502951-4ab25b5ca889?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80) center/cover" />
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="60px 0" sm-padding="40px 0">
+			<SocialMedia
+				facebook="https://www.facebook.com/quarklyapp/"
+				twitter="https://twitter.com/quarklyapp"
+				youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+				vkontakte="https://vk.com/quarklyapp"
+				telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+			>
+				<Override
+					slot="link"
+					border-radius="50%"
+					color="--light"
+					margin="0 8px"
+					background="--color-grey"
+					hover-background="--color-greyD1"
+				/>
+			</SocialMedia>
+		</Section>
 		<Section background-color="--dark" text-align="center" padding="32px 0">
 			<Menu
 				display="flex"
@@ -80,7 +314,10 @@ export default (() => {
 		</Link>
 		<RawHtml>
 			<script place={"endOfBody"} rawKey={"6009da73af03b56eef1f96d0"}>
-				{"<script>\nfunction ready() {\n        require(['jquery'], function ($) {\n                popupWhatsApp = () => {\n                        let btnClosePopup = document.querySelector('.closePopup');\n                        let btnOpenPopup = document.querySelector('.whatsapp-button');\n                        let popup = document.querySelector('.popup-whatsapp');\n                        let sendBtn = document.getElementById('send-btn');\n                        btnClosePopup.addEventListener(\"click\", () => {\n                                popup.classList.toggle('is-active-whatsapp-popup')\n                        })\n                        btnOpenPopup.addEventListener(\"click\", () => {\n                                popup.classList.toggle('is-active-whatsapp-popup')\n                                popup.style.animation = \"fadeIn .6s 0.0s both\";\n                        })\n                        sendBtn.addEventListener(\"click\", () => {\n                                let msg = document.getElementById('whats-in')\n                                        .value;\n                                let relmsg = msg.replace(/ /g, \"%20\");\n                                // Замените демо номер на свой — 79999999999\n                                window.open('https://wa.me/79999999999?text=' + relmsg, '_blank');\n                        });\n                        setTimeout(() => {\n                                popup.classList.toggle('is-active-whatsapp-popup');\n                        }, 2000); // <-Время появлявления формы WhatsApp 5000 = 5 секундам\n                }\n                popupWhatsApp();\n        });\n}\ndocument.addEventListener(\"DOMContentLoaded\", ready);\n\n</script>\n<div class=\"nav-bottom\">\n       \n<div class=\"popup-whatsapp fadeIn\">\n               \n<div class=\"content-whatsapp -top\">\n                       \n<button type=\"button\" class=\"closePopup\">\n                               \n<i class=\"material-icons icon-font-color\">\nclose\n</i>\n                       \n</button>\n                       \n<p>\nЗдравствуйте! Напишите нам сообщение в WhatsApp\n</p>\n               \n</div>\n               \n<div class=\"content-whatsapp -bottom\">\n                       \n<input class=\"whats-input\" id=\"whats-in\" type=\"text\" Placeholder=\"Написать сообщение...\" />\n                       \n<button class=\"send-msPopup\" id=\"send-btn\" type=\"button\">\n                               \n<i class=\"material-icons icon-font-color--black\">\nsend\n</i>\n                       \n</button>\n               \n</div>\n       \n</div>\n       \n<button type=\"button\" id=\"whats-openPopup\" class=\"whatsapp-button\">\n               \n<img class=\"icon-whatsapp\" src=\"https://image.flaticon.com/icons/svg/134/134937.svg\">\n       \n</button>\n</div>\n\n<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">\n<style>\n@import url('https://fonts.googleapis.com/css?family=Roboto');\n\n.nav-bottom {\n        display: flex;\n        flex-direction: row;\n        justify-content: flex-end;\n        align-content: flex-end;\n        width: auto;\n        height: auto;\n        position: fixed;\n        z-index: 999999999999999;\n        bottom: 80px;\n        right: 20px;\n        padding: 5px;\n        margin: 0px;\n}\n\n.whatsapp-button {\n        display: flex;\n        justify-content: center;\n        align-content: center;\n        width: 60px;\n        height: 60px;\n        z-index: 999999999;\n        transition: .3s;\n        margin: 10px;\n        padding: 7px;\n        border: none;\n        outline: none;\n        cursor: pointer;\n        border-radius: 50%;\n        background-color: rgb(255, 255, 255);\n        box-shadow: 1px 7px 25px -2px rgba(68, 68, 68, 0.28);\n}\n\n.popup-whatsapp {\n        display: none;\n        position: absolute;\n        flex-direction: column;\n        justify-content: flex-start;\n        align-items: flex-start;\n        min-width: 300px;\n        width: auto;\n        height: auto;\n        padding: 16px;\n        bottom: 100px;\n        right: 10px;\n        transition: .5s;\n        border-radius: 10px;\n        background-color: rgb(255, 255, 255);\n        animation: slideInRight .6s 0.0s both;\n        box-shadow: 0 16px 34px -10px rgba(0, 0, 0, 0.46);\n}\n\n.content-whatsapp {\n        display: flex;\n        flex-direction: column;\n        width: 100%;\n}\n\n.content-whatsapp p {\n        color: rgb(88, 88, 88);\n        font-family: 'Roboto';\n        font-weight: 400;\n        font-size: 1.0em;\n}\n\n.closePopup {\n        display: flex;\n        justify-content: center;\n        align-items: center;\n        width: 28px;\n        height: 28px;\n        margin: 0px 0px 15px 0px;\n        border-radius: 50%;\n        border: none;\n        outline: none;\n        cursor: pointer;\n        background-color: rgb(247, 96, 96);\n}\n\n.is-active-whatsapp-popup {\n        display: flex;\n        animation: slideInRight .6s 0.0s both;\n}\n\ninput#whats-in {\n        background-color: #f3f3f3;\n        outline: none;\n        border: none;\n        border-radius: 6px;\n        overflow: auto;\n        margin: 0px;\n        width: auto;\n        resize: none;\n        padding: 11px;\n}\n\n.icon-whatsapp-small {\n        width: 24px;\n        height: 24px;\n}\n\n.icon-whatsapp {\n        width: 45px;\n        height: 45px;\n}\n\n.icon-font-color {\n        color: #ffffff\n}\n\n.icon-font-color--black {\n        color: #333333\n}\n\ni.material-icons.icon-font-color--black {\n        color: #2cb742 !important;\n        font-style: normal;\n        font-size: 24px;\n        margin-left: 5px;\n}\n\nbutton#send-btn {\n        display: flex;\n        justify-content: center;\n        align-items: center;\n        width: 40px;\n        height: 40px;\n        background-color: #f3f3f3;\n        margin: 11px 0px 0px 5px;\n        border: none;\n        outline: none;\n        cursor: pointer;\n        border-radius: 50px;\n        color: white !important;\n}\n\n@media screen and (max-width: 768px) {\n        .popup-whatsapp {\n                min-width: auto !important;\n                width: auto !important;\n                height: auto !important;\n        }\n\n        .nav-bottom {\n                width: auto !important;\n                height: auto !important;\n                bottom: 0px !important;\n                right: 10px !important;\n        }\n}\n\n.popup-whatsapp:before {\n        content: '';\n        display: block;\n        width: 0;\n        height: 0;\n        position: absolute;\n        right: 26px;\n        z-index: 0;\n        border: 10px solid rgba(255, 255, 255, 0);\n        border-top: 10px solid #feffff;\n        bottom: -20px;\n}\n\n</style>\n\n"}
+				{"<script>\nSmoothScroll({\n    // Время скролла 400 = 0.4 секунды\n    animationTime    : 800,\n    // Размер шага в пикселях \n    stepSize         : 75,\n\n    // Дополнительные настройки:\n    \n    // Ускорение \n    accelerationDelta : 30,  \n    // Максимальное ускорение\n    accelerationMax   : 2,   \n\n    // Поддержка клавиатуры\n    keyboardSupport   : true,  \n    // Шаг скролла стрелками на клавиатуре в пикселях\n    arrowScroll       : 50,\n\n    // Pulse (less tweakable)\n    // ratio of \"tail\" to \"acceleration\"\n    pulseAlgorithm   : true,\n    pulseScale       : 4,\n    pulseNormalize   : 1,\n\n    // Поддержка тачпада\n    touchpadSupport   : true,\n})\n</script>\n\n"}
+			</script>
+			<script place={"endOfHead"} rawKey={"6009de00d53b0e744cc0abc0"}>
+				{"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.10/SmoothScroll.min.js\" integrity=\"sha256-huW7yWl7tNfP7lGk46XE+Sp0nCotjzYodhVKlwaNeco=\" crossorigin=\"anonymous\">\n</script>\n"}
 			</script>
 		</RawHtml>
 	</Theme>;
